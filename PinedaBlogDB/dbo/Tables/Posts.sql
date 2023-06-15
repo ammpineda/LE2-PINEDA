053ee81f7d7070181720a004736fe213aa55be10
ammpineda <115529731+ammpineda@users.noreply.github.com>
2023-06-16 03:52:38 +0800

    Delete PinedaBlogDB directory

--- PinedaBlogDB/dbo/Tables/Posts.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Posts] (
-    [Id]           INT            IDENTITY (1, 1) NOT NULL,
-    [UserId]       INT            NOT NULL,
-    [Title]        NVARCHAR (150) NOT NULL,
-    [Body]         TEXT           NOT NULL,
-    [DateCreated] DATETIME2 (7)  NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Posts_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
-);
